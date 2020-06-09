4e5c27e0269569320a309d168295414156efb54e
Vova <volodimir.partytskyi@gmail.com>
2020-06-09 17:13:33 +0300

    Add armor to enemy

--- src/Actors/Enemy/Enemy.gd
+++ src/Actors/Enemy/Enemy.gd
@@ -1,10 +1,14 @@
 extends Area2D
 
 export (int) var SPEED = 40
+export (int) var ARMOR = 3
 
 func _process(delta: float) -> void:
 	position.x -= SPEED * delta
 
 func _on_Enemy_body_entered(body: Node) -> void:
-	queue_free()
 	body.queue_free()
+
+	ARMOR -= 1
+	if ARMOR <= 0:
+		queue_free()
