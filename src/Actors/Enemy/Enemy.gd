b2dc0b30d31c238c3f9c5223b20a5af47b829221
Vova <volodimir.partytskyi@gmail.com>
2020-06-10 13:08:25 +0300

    Update bullet hit effect

--- src/Actors/Enemy/Enemy.gd
+++ src/Actors/Enemy/Enemy.gd
@@ -5,6 +5,7 @@ export (int) var ARMOR = 3
 export (int) var SCORE_UP = 10
 
 const ExplosionEffect: Object = preload("res://src/Actors/ExplosionEffect.tscn")
+const HitEffect = preload("res://src/Actors/HitEffect.tscn")
 
 func _process(delta: float) -> void:
 	position.x -= SPEED * delta
@@ -17,6 +18,7 @@ func score_up() -> void:
 
 func _on_Enemy_body_entered(body: Node) -> void:
 	body.queue_free()
+	create_hit_effect()
 
 	ARMOR -= 1
 	if ARMOR <= 0:
@@ -29,3 +31,9 @@ func create_explosion() -> void:
 	var main = get_tree().current_scene
 	main.add_child(explosion)
 	explosion.global_position = global_position
+
+func create_hit_effect() -> void:
+	var hit_effect = HitEffect.instance()
+	var main = get_tree().current_scene
+	main.add_child(hit_effect)
+	hit_effect.global_position = global_position
